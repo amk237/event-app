1021eff35ad80cbe3af7818fa94a031a0553e209
Animesh Tirkey <147409238+sleepypant@users.noreply.github.com>
2025-11-06 12:41:50 -0700

    Animesh (#86)
    * Implmented US 02.04.01 (Organizer Story)
    
    * Change for merge conflict
    
    * Prev version
    
    * Implemented US 02.04.01
    
    * buttons with setEnabled functionality
    
    * Changed App Icon(new)
    
    * Fix duplicate key names in strings.xml
    
    * changed app icon and page pop up
    
    * Update README.md
    
    I have 2 account on this project. I don't even know why
    
    * QR code generated feature
    
    * IMplemented time and creation of event parameters
    
    * Add EventCreationActivity and enhance event validation
    
    - Introduced EventCreationActivity in AndroidManifest.xml.
    - Improved form validation in EventCreationActivity to include checks for start date, end date, registration open, and close dates.
    - Updated Event model to include location property with corresponding getter and setter methods.
    
    * Add event creation strings to strings.xml
    
    - Added strings for event creation success message, QR code description, and button labels (Done, Save, Share) to enhance user experience in the Event Created Activity.
    
    * correcting gradle file
    
    ---------
    
    Co-authored-by: Gskakar <145719047+Gskakar@users.noreply.github.com>
    Co-authored-by: sleepsonrockss <animoanimo197@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.event_app"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.event_app"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -27,8 +27,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
@@ -43,14 +43,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation(platform("com.google.firebase:firebase-bom:34.5.0")) // Use the latest version
-    implementation("com.google.firebase:firebase-auth:24.0.1")
-    implementation("com.google.firebase:firebase-messaging:25.0.1")
-    implementation("com.google.firebase:firebase-firestore")
-
-
-    implementation(libs.appcompat)
-    implementation(libs.material)
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-messaging")
     implementation(libs.zxing.android.embedded)
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
