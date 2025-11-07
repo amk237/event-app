c7286c71ddf5a002f0f539c62941f13df92e28a0
Amaan K <amaank9181@gmail.com>
2025-11-07 06:48:54 -0700

    Update ExampleUnitTest.java
    Unit test update

--- app/src/test/java/com/example/event_app/ExampleUnitTest.java
+++ app/src/test/java/com/example/event_app/ExampleUnitTest.java
@@ -14,4 +14,9 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void basicMathTest() {
+        assertTrue("Basic assertion should pass", 1 + 1 == 2);
+    }
 }
